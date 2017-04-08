ec8e2413969a5ccd723d8746739a927da92ade75
rrozansk <nrr1228@gmail.com>
2017-04-08 12:08:56 -0700

    organized conf file to be more readable to humans and more parser friendly to the ini bnf grammar

--- include/ini.h
+++ include/ini.h
@@ -6,10 +6,10 @@
 /*
  Author:  Ryan Rozanski
  Created: 3/27/17
- Edited:  4/6/17
- Info:    A ini conf file interface. Allows static reading a from file as well as
+ Edited:  4/7/17
+ Info:    A ini conf file interface. Allows static reading from a file as well as
           dynamic creation, mutation, and output to a file. Below is the grammar
-          accepted and output by the library.
+          accepted, and output by the library.
 
                  ::BNF GRAMMAR::
 
@@ -47,26 +47,26 @@ typedef struct ini ini_t; // INI conf files
     F U N C T I O N   P R O T O T Y P E S
 
 *******************************************************************************/
-// read in and create an ini conf given a file name. return NULL on success and
+// Read in and create an ini conf given a file name. Return NULL on success and
 // a descriptive error message on failure.
 const char *readINI(ini_t **ini, char *fname);
 
-// output the ini conf to a file with the specified name. 0 on success, 1 on fail.
+// Output the ini conf to a file with the specified name. 0 on success, 1 on fail.
 int writeINI(ini_t *ini, char *fname);
 
-// attempt to create an new ini conf. return NULL upon failure.
+// Attempt to create an new ini conf. Return NULL upon failure.
 ini_t *makeINI();
 
-// free the ini conf.
+// Free the ini conf.
 void freeINI(ini_t *ini);
 
-// retrieve a value from the conf given by section and key.
-// return NULL if any of: ini, section, or key are NULL, or if not found.
+// Retrieve a value from the conf given by section and key.
+// Return NULL if ini, section, or key is NULL, or if not found.
 char *getINI(ini_t *ini, char *section, char *key);
 
-// create a new setting in the conf under the specified section with the given 
-// key and value. return 0 if successful, 1 on failure. failure occurs if ini,
-// section, key, or val is NULL or cannot alloc space. failure can also occur if
+// Create a new setting in the conf under the specified section with the given 
+// key and value. Return 0 if successful, 1 on failure. Failure occurs if ini,
+// section, key, or val is NULL or cannot alloc space. Failure can also occur if
 // section, key, or val do not conform to the grammar given above.
 int setINI(ini_t *ini, char *section, char *key, char *val);
 
